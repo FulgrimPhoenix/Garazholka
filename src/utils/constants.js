e494f78bba102ee43384ce84a253f0b38d0b7018
Ivanyuschin Ilia <ivanyuschin.work@yandex.ru>
2024-09-04 23:27:25 +0300

    feat: start developing group list

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,30 +5,41 @@ import tyranids from "../images/intro/tyranids.jpg";
 import guard from "../images/intro/Guard.jpg";
 import logo from "../images/forms/xmark.svg";
 import backButton from "../images/forms/backButton.svg";
-import menuIcon from "../images/header/menuIcon.svg"
+import menuIcon from "../images/header/menuIcon.svg";
 
 export const constants = {
   header: {
     logo: "Garazholka",
     buttons: ["Войти", "Регистрация"],
-    menuIcon
+    menuIcon,
   },
   registerFormData: {
     logo: logo,
     title: "Регистрация",
     buttonTextFirstPage: "Далее",
     buttonTextSecondPage: "Зарегистрироваться",
-    backButton
+    backButton,
   },
   loginFormData: {
     logo: logo,
     title: "Войти",
     buttonText: "Войти",
   },
-  profile:{
+  profile: {
     name: "Райан Гослинг",
-    aboutMe: "Кенодостаточен. Известен по ролям в независимых фильмах и крупных студийных проектах разных жанров.",
-    avatar: "https://cdna.artstation.com/p/assets/images/images/037/619/882/large/sean-kelly-officer-k1.jpg"
+    aboutMe:
+      "Кенодостаточен. Известен по ролям в независимых фильмах и крупных студийных проектах разных жанров.",
+    avatar:
+      "https://cdna.artstation.com/p/assets/images/images/037/619/882/large/sean-kelly-officer-k1.jpg",
+    groupButtonText: "Группы",
+    groupList: [
+      { avatar: "", id: 1, title: "Первая" },
+      { avatar: "", id: 2, title: "Вторая" },
+      { avatar: "", id: 3, title: "Третья" },
+      { avatar: "", id: 4, title: "Четвертый" },
+      { avatar: "", id: 5, title: "Пятый" },
+      { avatar: "", id: 6, title: "Шестой" },
+    ],
   },
   intro: {
     title: "Мероприятие дня:",
