47ac5fb3e70d87cac62ff7eb1bdae223edfe24b7
Fulgrim <ivanyuschin.work@yandex.ru>
2024-09-07 12:21:35 +0300

    feat: develop eventlist in profile

--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -1,7 +1,19 @@
 import { NavLink } from "react-router-dom";
 import "./EventList.css";
+import { useState } from "react";
 
 export function EventList({ eventListData }) {
+  const [abbreviatedEventList, setAbbreviatedEventList] = useState(
+    eventListData.eventList.filter((el, i) => i < 5)
+  );
+  const [likedEventList, setLikedEventList] = useState([]);
+
+  function takeFormValues(e) {
+    likedEventList.includes(e.target.id)
+      ? likedEventList.splice(likedEventList.indexOf(e.target.id), 1)
+      : setLikedEventList([...likedEventList, e.target.id]);
+  }
+
   return (
     <div className="event-list">
       <NavLink to={"#"} className={"event-list__link"}>
@@ -12,7 +24,27 @@ export function EventList({ eventListData }) {
           alt="стрелка статуса меню"
         />
       </NavLink>
-      <ul className="event-list__list"></ul>
+      <ul className="event-list__items">
+        {abbreviatedEventList.map((el) => {
+          return (
+            <li className="event-list__item">
+              <input
+                onClick={(e) => {
+                  takeFormValues(e);
+                }}
+                key={el.id}
+                id={el.id}
+                className="event-list__item-input"
+                type="checkbox"
+                name={el.title}
+              />
+              <label htmlFor={el.id} className="event-list__item-label">
+                <span className="event-list__item-title">{el.title}</span>
+              </label>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
