6e2c99aee338b0ba296542f5cde8c146f860711d
Fulgrim <ivanyuschin.work@yandex.ru>
2024-09-09 18:35:00 +0300

    feat: develop way time preference block

--- src/components/WayTimePreferenceBlock/WayTimePreferenceBlock.js
+++ src/components/WayTimePreferenceBlock/WayTimePreferenceBlock.js
@@ -19,6 +19,17 @@ export function WayTimePreferenceBlock({ wayTimePreferenceBlockData }) {
           {wayTimePreferenceBlockData.isOnlineCheckboxTitle}
         </span>
       </div>
+      <ul className="way-time-preference-block__way-time-items">
+        {wayTimePreferenceBlockData.wayTimeItems.map((item) => (
+          <li className="way-time-preference-block__way-time-item" key={`1${item.id}`}>
+            <input className="way-time-preference-block__way-time-input-item" key={`2${item.id}`} id={item.id} type="radio" name="wayTime" />
+            <label className="way-time-preference-block__way-time-label-item" key={`3${item.id}`} htmlFor={item.id}>
+              
+            </label>
+            <span className="way-time-preference-block__way-time-title-item">{item.title}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
