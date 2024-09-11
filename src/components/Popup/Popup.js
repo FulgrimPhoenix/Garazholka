c5ebe24fde68e0c4cc2efe3674a0166dd4423768
Fulgrim <ivanyuschin.work@yandex.ru>
2024-09-11 19:03:37 +0300

    feat: develop close func on swipe bot

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,15 +1,43 @@
+import { useState } from "react";
 import "./Popup.css";
+import PopupExitSwipe from "../../hooks/PopupExitSwipe";
 
 export function Popup({ children, togglePopup, isPopupOpen }) {
+  const [isCloseAnimationPlay, setIsCloseAnimationPlay] = useState(false);
+  const { isModalOpen, setIsModalOpen, ref } = PopupExitSwipe();
+
+  function closeModal() {
+    togglePopup();
+    setIsModalOpen(!isModalOpen);
+  }
+
+  function toggleCloseAnimationPlay() {
+    setIsModalOpen(!isModalOpen);
+    setIsCloseAnimationPlay(!isCloseAnimationPlay);
+  }
+
   return (
     <div
-      className={`popup ${isPopupOpen ? "popup_open" : ""}`}
-      onClick={(e) =>
-        e.target == document.querySelector(".popup") ? togglePopup() : ""
-      }
+      className={`popup ${isModalOpen ? "popup_open" : ""} ${
+        isCloseAnimationPlay ? "popup_closing" : ""
+      }`}
+      onClick={(e) => {
+        if (e.target === document.querySelector(".popup")) {
+          closeModal();
+          toggleCloseAnimationPlay();
+          setTimeout(() => {
+            toggleCloseAnimationPlay();
+          }, 400);
+        }
+      }}
       name="popupOverlay"
     >
-      <div className="popup__container">
+      <div
+        className={`popup__container ${
+          isModalOpen ? "" : "popup__container_closing"
+        }`}
+        ref={ref}
+      >
         <h3 className="popup__title">Hello</h3>
         <p className="popup__description">Lalalalalala</p>
         {children}
