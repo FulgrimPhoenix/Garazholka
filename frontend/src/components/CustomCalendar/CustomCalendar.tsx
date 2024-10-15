1a56a73b021612f46999030055064864e6bab776
Fulgrim <ivanyuschin.work@yandex.ru>
2024-10-15 14:18:52 +0300

    feat: add calendar display to modal window

--- frontend/src/components/CustomCalendar/CustomCalendar.tsx
+++ frontend/src/components/CustomCalendar/CustomCalendar.tsx
@@ -3,6 +3,8 @@ import { createYear } from "../../utils/helpers/date/createYear";
 import { useCalendar } from "../../hooks/useCalendar";
 import { formateDate } from "../../utils/helpers/date/formateDate";
 import "./CustomCalendar.css";
+import { checkToday } from "../../utils/helpers/date/checkToday";
+import { checkDateIsEqual } from "../../utils/helpers/date/checkDateIsEqual";
 
 interface ICustomCalendar {
   locale?: string;
@@ -13,7 +15,7 @@ interface ICustomCalendar {
 const CustomCalendar = ({ locale }: ICustomCalendar): React.ReactElement => {
   const [selectedDate, setSelectedDate] = React.useState(new Date());
 
-  const { state } = useCalendar({
+  const { state, functions } = useCalendar({
     firstWeekDay: 2, //с какого дня начинается неделя
     locale,
     date: selectedDate,
@@ -30,13 +32,57 @@ const CustomCalendar = ({ locale }: ICustomCalendar): React.ReactElement => {
         <h2 className="calendar__header">
           <div aria-hidden className="calentar__header-arrow-left" />
           {state.mode === "days" && (
-            <div>
+            <div onClick={() => functions.setMode("monthes")}>
               {state.monthesNames[state.selectedMonth.monthIndex].monthShort}{" "}
+              {state.selectedDate.year}
+            </div>
+          )}
+          {state.mode === "monthes" && (
+            <div onClick={() => functions.setMode("days")}>
               {state.selectedDate.year}
             </div>
           )}
           <div aria-hidden className="calentar__header-arrow-right" />
         </h2>
+        <div className="calendar__body">
+          {state.mode === "days" && (
+            <>
+              <div className="calendar__week-days">
+                {state.weekDaysNames.map((weekDaysName) => (
+                  <div key={weekDaysName.dayShort}>{weekDaysName.dayShort}</div>
+                ))}
+              </div>
+              <div className="calendar__days">
+                {state.calendarDays.map((day) => {
+                  const isToday = checkToday(day.date);
+                  const isSelectedDay = checkDateIsEqual({
+                    date1: day.date,
+                    date2: state.selectedDate.date,
+                  });
+                  const isAdditionalDay =
+                    day.monthIndex !== state.selectedMonth.monthIndex;
+                  console.log(day);
+
+                  return (
+                    <div
+                      key={`${day.dayNumber}-${day.monthIndex}-${day.year}`}
+                      className={[
+                        "calendar__day",
+                        isToday ? "calendar__today-day" : "",
+                        isSelectedDay ? "calendar__selected-day" : "",
+                        isAdditionalDay ? "calendar__additional-day" : "",
+                      ].join(" ")}
+                    >
+                      <span className="calendar__day-date">
+                        {day.dayNumber}
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
